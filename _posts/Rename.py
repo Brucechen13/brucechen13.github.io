4966f64143fb5d2ce4b9c9541a0d53739d635b57
Brucechen13 <chenchi_94@163.com>
2017-04-01 09:25:16 +0800

    upadte blogs

--- _posts/Rename.py
+++ _posts/Rename.py
@@ -4,15 +4,20 @@ import os
 import os.path
 import re
 date_time = date.today()
-print date_time.strftime('%Y-%m-%d')
-pattern = re.compile("\d{4}-\d{2}-\d{2}-.+", re.I)
-for root, dirs, files in os.walk("./"):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
-    for file in files:                        #输出文件信息
-        print "parent is" + root
-        print "filename is:" + file
-        if re.match(pattern, file) or file.endswith("py"):
-            print "normal file"
-        else:
-            oldsrc = root + file
-            newsrc = root + date_time.strftime('%Y-%m-%d') + '-' + file
-            os.rename(oldsrc, newsrc)
+strText = '''---
+layout:     post
+title:      "笔记"
+subtitle:   ""
+date:       %s
+author:     "brucechen"
+header-img: "img/post-bg-java.jpg"
+published: false
+tags:
+    - Java
+    - 读书笔记
+---''' % date_time.strftime('%Y-%m-%d')
+print(date_time.strftime('%Y-%m-%d'))
+f = open('%s.markdown'%date_time.strftime('%Y-%m-%d'), mode='w')
+f.write(strText)
+f.close()
+print(strText)
